9ca70c76c839db98ce25be8bd2980875e8c4bdb2
Joshua Singleton <jsingle345@gmail.com>
2020-09-10 00:24:36 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,7 +47,7 @@ def checkout(cart, coupons)
   total = 0 
   
   while counter < final_cart.length 
-    total = final_cart[counter][:price] * final_cart[counter][:count]
+    total += final_cart[counter][:price] * final_cart[counter][:count]
     if total > 100
       total -= total * 0.10
     end
