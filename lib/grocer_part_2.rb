26988aa1a3311dae37f6de293f7b74d9704596dd
Joshua Singleton <jsingle345@gmail.com>
2020-09-10 00:16:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -40,5 +40,11 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  clearanced_cart = apply_clearance(couponed_cart)
+  
+  counter = 0 
+  total = 0 
+  
+  while counter < 
 end
